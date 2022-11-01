fdf6dfd9bbc9164fdfdd98c262412765b632cb48
Maksym Lytvyn <makshow.at@gmail.com>
2022-11-01 13:51:59 +0200

    feat: dynamic job page

--- jobboard/pages/job/[id].tsx
+++ jobboard/pages/job/[id].tsx
@@ -9,6 +9,7 @@ import placeIcon from '../../assets/icons/place.svg';
 import arrowIcon from '../../assets/icons/arrow.svg';
 import map from '../../assets/map.png';
 import Link from 'next/link';
+import { formatDistance } from 'date-fns';
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paramsArray = jobsData.map((job) => ({ params: { id: job.id } }));
@@ -53,31 +54,27 @@ export default function Job(props: JobDetails) {
 
         <div className="mt-8 flex flex-col justify-between xl:flex-row">
           <h2 className="max-w-[501px] text-2xl font-bold leading-tight tracking-tight">
-            Arbeitsmediziner/-in / Betriebsmediziner/-in (m/w/d) oder einen Arzt/eine Ärztin (m/w/d)
-            für die Weiterbildung zum Facharzt/ zur Fachärztin für Arbeitsmedizin (m/w/d)
+            {props.title}
           </h2>
           <div className="mt-2 flex flex-row items-center justify-between xl:items-start">
             <div className="font-secondary text-[13px] font-light text-txt-dark/60 sm:font-secondary sm:text-lg sm:text-txt-dark/40 xl:hidden">
-              Posted 2 days ago
+              Posted{' '}
+              {formatDistance(new Date(`${props.createdAt}`), new Date(), { addSuffix: true })}
             </div>
             <div className="flex flex-col gap-1 text-right xl:gap-0 xl:text-left">
               <div className="text-lg tracking-tight text-txt-dark/80 xl:order-last">
                 Brutto, per year
               </div>
-              <div className="text-xl font-bold leading-tight tracking-tight">€ 54 000—60 000</div>
+              <div className="text-xl font-bold leading-tight tracking-tight">{`€ ${props.salary}`}</div>
             </div>
           </div>
         </div>
 
         <div className="mt-2 hidden font-secondary text-lg text-txt-dark/40 xl:block">
-          Posted 2 days ago
+          Posted {formatDistance(new Date(`${props.createdAt}`), new Date(), { addSuffix: true })}
         </div>
         <div className="mt-2 text-lg leading-tight tracking-tight sm:font-secondary">
-          At WellStar, we all share common goals. That’s what makes us so successful – and such an
-          integral part of our communities. We want the same things, for our organization, for our
-          patients, and for our colleagues. As the most integrated healthcare provider in Georgia,
-          this means we pride ourselves on investing in the communities that we serve. We continue
-          to provide innovative care models, focused on improving quality and access to healthcare.
+          {props.description}
         </div>
 
         <button className="m-auto mt-[39px] flex items-center rounded-lg bg-dark-01 py-[18px] px-[30px] text-xs uppercase text-white transition-all hover:bg-slate-800 hover:shadow-md sm:mx-0">
@@ -85,34 +82,20 @@ export default function Job(props: JobDetails) {
         </button>
 
         <div className="flex flex-col">
-          <section className="mt-[135px] sm:mt-[86px] flex flex-col gap-2 overflow-x-auto">
+          <section className="mt-[135px] flex flex-col gap-2 overflow-x-auto sm:mt-[86px]">
             <h3 className="text-[28px] font-bold tracking-wide">Attached images</h3>
             <hr className="" />
             <div className="mt-[13px] flex flex-none gap-[10px]">
-              <picture className="flex flex-none">
-                <source srcSet={'https://picsum.photos/200/300'} type="image/jpg" />
-                <img
-                  className="h-[115px] w-[209px] rounded-lg object-cover"
-                  src={'https://picsum.photos/200/300'}
-                  alt="Photo"
-                />
-              </picture>
-              <picture className="flex flex-none">
-                <source srcSet={'https://picsum.photos/200/300'} type="image/jpg" />
-                <img
-                  className="h-[115px] w-[209px] rounded-lg object-cover"
-                  src={'https://picsum.photos/200/300'}
-                  alt="Photo"
-                />
-              </picture>
-              <picture className="flex flex-none">
-                <source srcSet={'https://picsum.photos/200/300'} type="image/jpg" />
-                <img
-                  className="h-[115px] w-[209px] rounded-lg object-cover"
-                  src={'https://picsum.photos/200/300'}
-                  alt="Photo"
-                />
-              </picture>
+              {props.pictures.map((pic, index) => (
+                <picture key={index} className="flex flex-none">
+                  <source srcSet={pic} type="image/jpg" />
+                  <img
+                    className="h-[115px] w-[209px] rounded-lg object-cover"
+                    src={pic}
+                    alt="Photo"
+                  />
+                </picture>
+              ))}
             </div>
           </section>
 
@@ -123,25 +106,26 @@ export default function Job(props: JobDetails) {
               Employment type
             </div>
             <div className="flex gap-2">
-              <div className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#55699E]/30 bg-[#A1B1DB]/30 px-2 text-center text-[16px] font-bold leading-none text-[#55699E]">
-                Full time
-              </div>
-              <div className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#55699E]/30 bg-[#A1B1DB]/30 px-2 text-center text-[16px] font-bold leading-none text-[#55699E]">
-                Part time
-              </div>
-              <div className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#55699E]/30 bg-[#A1B1DB]/30 px-2 text-center text-[16px] font-bold leading-none text-[#55699E]">
-                Temporary
-              </div>
+              {props.employment_type.map((type, index) => (
+                <div
+                  key={index}
+                  className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#55699E]/30 bg-[#A1B1DB]/30 px-2 text-center text-[16px] font-bold leading-none text-[#55699E]"
+                >
+                  {type}
+                </div>
+              ))}
             </div>
 
             <div className="mt-4 font-secondary text-lg leading-tight tracking-tight">Benefits</div>
             <div className="flex gap-2">
-              <div className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#FFCF00] bg-[#FFCF00]/20 px-2 text-center text-[16px] font-bold leading-none text-[#988B49]">
-                Flexible schedule
-              </div>
-              <div className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#FFCF00] bg-[#FFCF00]/20 px-2 text-center text-[16px] font-bold leading-none text-[#988B49]">
-                Relocation assistance
-              </div>
+              {props.benefits.map((benefit, index) => (
+                <div
+                  key={index}
+                  className="flex h-[49px] max-w-[222px] flex-auto items-center justify-center rounded-lg border border-solid border-[#FFCF00] bg-[#FFCF00]/20 px-2 text-center text-[16px] font-bold leading-none text-[#988B49]"
+                >
+                  {benefit}
+                </div>
+              ))}
             </div>
           </section>
         </div>
@@ -162,14 +146,14 @@ export default function Job(props: JobDetails) {
         <span className="absolute h-[273px] w-[273px] translate-y-[-12px] translate-x-[-77px] rounded-full bg-[#202336]"></span>
         <div className="relative m-auto mt-[31px] flex flex-col gap-2 px-16">
           <div className="text-[20px] font-bold leading-tight text-[##E7EAF0]">
-            Department name. University Hospital Giessen.
+            {props.name}
           </div>
           <div className="font-secondary text-lg leading-snug tracking-tight text-[#E8EBF3]">
             <Image src={placeIcon} alt="P" className="mr-2 inline -translate-y-1" />
-            AKH Wien, 1090 Wien, Währinger Gürtel 18-20
+            {props.address}
           </div>
           <div className="font-secondary text-lg leading-snug tracking-tight text-[#E8EBF3]">
-            +43 (01) 40400-12090, post_akh_diz@akhwien.at
+            {`${props.phone}, ${props.email}`}
           </div>
         </div>
         <Image src={map} alt="P" className="relative mt-5 rounded-b-lg" />
